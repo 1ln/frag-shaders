b5f1ddb80e37f0748d9a7d0de5ca296975415ebc
dan olson <dano7900@gmail.com>
2020-03-12 16:21:09 -0400

    update settings

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,19 +4,42 @@ void ofApp::setup() {
 
 shader.load("render.vert","render.frag");
 
-w = ofGetWidth();
-h = ofGetHeight();
+cam_orbit = true;
+
+steps = 64;
+eps = 0.0001;
+trace_dist = 500.0;
+
+cam.setTarget(glm::vec3(0.0));
+
+mouse.x = 0;
+mouse.y = 0;
+
+//light.setPosition(ofVec3f(10.0));
 
 }
 
 void ofApp::draw() {
 
+cam.setPosition(0.0,0.0,-5.0);
+
+if(cam_orbit == true) {
+cam.begin();
+cam.end();
+}
+
 shader.begin();
 
 shader.setUniform1f("u_time",ofGetElapsedTimeMillis());
-shader.setUniform2f("u_res",w,h);
+shader.setUniform1i("u_steps",steps);
+shader.setUniform1f("u_trace_dist",trace_dist);
+shader.setUniform1f("u_eps",eps);
+shader.setUniform2f("u_res",ofGetWidth(),ofGetHeight());
+shader.setUniform3f("u_cam_pos",cam.getPosition().x,cam.getPosition().y,cam.getPosition().z);
+shader.setUniform2f("u_mouse_pos",mouse.x,mouse.y);
+//shader.setUniform3fv("u_light_pos",ofVec3f(light.getPosition()));
 
-ofDrawRectangle(0,0,w,h);
+ofDrawRectangle(0,0,ofGetWidth(),ofGetHeight());
 
 shader.end();
 
@@ -25,3 +48,27 @@ shader.end();
 
 void ofApp::update() {
 }
+
+void ofApp::keyPressed(int key) {
+    if(key == 's') {
+        img.grabScreen(0,0,ofGetWidth(),ofGetHeight());
+        img.save("source.png");
+    }
+}
+
+void ofApp::mousePressed(int x,int y,int button) {
+}
+
+void ofApp::mouseReleased(int x,int y,int button) {
+}
+
+void ofApp::mouseMoved(int x,int y) {
+    mouse.x = x;
+    mouse.y = y;
+}
+
+
+
+
+
+
--- src/ofApp.h
+++ src/ofApp.h
@@ -8,12 +8,32 @@ class ofApp : public ofBaseApp {
 
     void setup();
     void draw();
-    void update();
+    void update();  
+
+    void keyPressed(int key);
+    void mouseMoved(int x,int y);   
+    void mousePressed(int x, int y,int button);
+    void mouseReleased(int x,int y,int button);
+        
+    ofEasyCam cam;
+
+    ofFbo buf;
+
+    ofTexture ntex;
+    ofTexture rtex;
 
     ofShader shader;
-    
-    int w;
-    int h;
+    ofImage img;      
+
+    ofNode light;
+
+    float trace_dist;
+    float eps;
+       
+    int steps;
+    bool cam_orbit;
+
+    ofVec2f mouse;
 
 };
 
