675481d49387fb3b6187694c1bcbb4bf364f9177
dan olson <dano7900@gmail.com>
2020-03-31 18:13:00 -0400

    update

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,6 +4,8 @@ void ofApp::setup() {
 
 shader.load("render.vert","frag.frag");
 
+s.allocate(512,512,GL_RGBA);
+
 w = ofGetWidth();
 h = ofGetHeight();
 
@@ -23,25 +25,25 @@ mouse.y = 0;
 
 gui.setup();
 
-gui.add(steps.set("Steps",100,0,1000));
-gui.add(eps.set("Epsilon",0.0001,0.0001,0.01));
-gui.add(dist.set("Distance",500,0,10000));
+gui.add(steps.set("Steps",250,0,2500));
+gui.add(eps.set("Epsilon",0.001,0.00001,.01));
+gui.add(dist.set("Distance",500,0,2500));
 
 gui.add(aa.set("Anti Alias",1,0,3));
 
-gui.add(shad_steps.set("Shadow Steps",25,0,1000));
-gui.add(shad_eps.set("Shadow Epsilon",0.001,0.0,1.));
-gui.add(shad_min.set("Shadow Minimum",0.005,0.,100.));
-gui.add(shad_max.set("Shadow Maximum",0.005,0.,100.));
+//gui.add(cam_pos.set("Camera position",glm::vec3(cam.getPosition()),""));
+//gui.add(cam_fov.set("Camera fov",45,0,2500));
+//gui.add(cam_orbit.set("Camera orbit",true));
+
+gui.add(shad_steps.set("Shadow Steps",25,0,500));
+gui.add(shad_eps.set("Shadow Epsilon",0.001,0.00001,0.01));
+gui.add(shad_min.set("Shadow Minimum",0.005,0.0,10.));
+gui.add(shad_max.set("Shadow Maximum",10.,0.,100.));
 gui.add(shad_k.set("Shadow K",10.,0.,100.));
 
-dif_listener = addListener(this,&ofApp::updateDif);
-amb_listener = addListener(this,&ofApp::updateAmb);
-spe_listener = addListener(this,&ofApp::updateSpe);
-
-gui.add(dif.set("Diffuse",ofColor(255.,0.,0.),ofColor(0.),ofColor(255.)));
-gui.add(amb.set("Ambient",ofColor(.05),ofColor(0.),ofColor(255.)));
-gui.add(spe.set("Specular",ofColor(1.),ofColor(0.),ofColor(255.)));
+gui.add(dif.set("Diffuse",glm::vec3(1.,0.,0.),glm::vec3(0.),glm::vec3(1.)));
+gui.add(amb.set("Ambient",glm::vec3(.05),glm::vec3(0.),glm::vec3(1.)));
+gui.add(spe.set("Specular",glm::vec3(1.),glm::vec3(0.),glm::vec3(1.)));
 
 gui.add(light_pos.set("Light Position",glm::vec3(1.0),glm::vec3(-1.),glm::vec3(1.)));
 gui.add(light_intensity.set("Light Intensity",10.0,0.0,100.0));
@@ -52,7 +54,7 @@ gui.add(plane_orient.set("Plane orient",glm::vec3(0.0,1.0,0.0),glm::vec3(0.),glm
 gui.add(plane_display.set("Plane Display",false));
 
 gui.add(screen_size.set("Screen Size",""));
-
+gui.add(fps_counter.set("Frame per second",""));
 
 }
 
@@ -62,6 +64,9 @@ if(cam_orbit == true) {
 cam.begin();
 cam.end();
 }
+
+s.begin();
+ofClear(0);
 
 shader.begin();
 
@@ -98,10 +103,14 @@ shader.setUniform3f("u_cam_tar",glm::vec3(cam.getTarget().getPosition()));
 
 shader.setUniform2f("u_mouse_pos",mouse.x,mouse.y);
 
+plane.set(w,h);
+plane.setPosition(w/2,h/2,0);
 plane.draw();
+
 //ofDrawRectangle(0,0,w,h);
 
 shader.end();
+s.end();
 
 if(!gui_hide) {
 gui.draw();
@@ -111,6 +120,8 @@ gui.draw();
 
 void ofApp::update() {
 
+fps_counter = ofToString(ofGetFrameRate());
+
 w = ofGetWidth();
 h = ofGetHeight();
 
@@ -119,6 +130,7 @@ h = ofGetHeight();
 void ofApp::windowResized(int w,int h) {
 
 screen_size = ofToString(w) + "," + ofToString(h);
+
 }
 
 void ofApp::keyPressed(int key) {
@@ -143,18 +155,6 @@ void ofApp::mouseReleased(int x,int y,int button) {
 void ofApp::mouseMoved(int x,int y) {
     mouse.x = x;
     mouse.y = y;
-}
-
-void ofApp::updateDif(ofColor & col) {
-    dif = col;
-}
-
-void ofApp::updateAmb(ofColor & col) {
-    amb = col;
-}
-
-void ofApp::updateSpe(ofColor & col) {
-    spe = col;
 }
 
 
