a119393bae680c852ab152d4180e60c2fda0cc3d
dan olson <dano7900@gmail.com>
2020-04-06 14:23:36 -0400

    update

--- src/ofApp.h
+++ src/ofApp.h
@@ -35,8 +35,11 @@ class ofApp : public ofBaseApp {
 
     ofParameterGroup render_grp;
     ofParameterGroup info_grp;
-    ofParameterGroup cam_grp;
+    ofParameterGroup sdf_grp;
     ofParameterGroup light_grp;
+    ofParameterGroup reflect_grp;
+    ofParameterGroup sine_grp;
+    ofParameterGroup fnoise_grp;
     ofParameterGroup shad_grp;
     ofParameterGroup plane_grp;
 
@@ -44,32 +47,46 @@ class ofApp : public ofBaseApp {
     ofParameter<float> eps;  
     ofParameter<int> steps;
     ofParameter<float> hash;   
-
-    ofParameter<glm::vec3> cam_pos;
-    ofParameter<bool> cam_orbit;
-    ofParameter<float> cam_fov;
-
     ofParameter<int> aa;
   
+    ofParameter<int> reflect_steps;
+    ofParameter<float> reflect_eps;
+    ofParameter<float> reflect_min;
+    ofParameter<float> reflect_max;
+
     ofParameter<int> shad_steps;
     ofParameter<float> shad_eps;
     ofParameter<float> shad_min;
     ofParameter<float> shad_max;
     ofParameter<float> shad_k;
 
+    ofParameter<glm::vec3> col;
     ofParameter<glm::vec3> dif;
     ofParameter<glm::vec3> amb;
     ofParameter<glm::vec3> spe;
-
     ofParameter<glm::vec3> light_pos;
     ofParameter<float> light_intensity;
     ofParameter<bool> light_display;
     ofParameter<bool> light_cam;
 
-    ofParameter<glm::vec3> plane_offset;
-    ofParameter<glm::vec3> plane_orient;
+    ofParameter<float> plane_offset;
     ofParameter<bool> plane_display;
 
+    ofParameter<bool> sine_display;
+    ofParameter<float> sine_height;   
+    ofParameter<float> sine_offset;
+
+    ofParameter<bool> fnoise_display;
+    ofParameter<int> fnoise_octaves;
+    ofParameter<float> fnoise_frequency;
+    ofParameter<float> fnoise_offset;
+
+    ofParameter<bool> sphere;
+    ofParameter<bool> box;
+    ofParameter<bool> torus;
+    ofParameter<bool> octahedron;
+    ofParameter<bool> prism;
+ 
     ofParameter<string> screen_size;
     ofParameter<string> fps_counter;
    
@@ -77,4 +94,3 @@ class ofApp : public ofBaseApp {
     ofVec2f mouse;
 
 };
-
