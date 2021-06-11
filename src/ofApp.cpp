23fb283daae9158f34fc41e5faf07a946a67d984
dan olson <dano7900@gmail.com>
2021-06-11 18:58:03 -0400

    update

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,19 +4,23 @@ void ofApp::setup() {
 
 ofDisableDepthTest();
 
-frag = "frag.frag"; 
-
 path = filesystem::path("../../src");
-shader.load(path/"render.vert",path/frag);
+render.load(path/"render.vert",path/frag.frag); 
 buffer.load(path/"render.vert",path/buffer.glsl);
 
 w = ofGetWidth(); 
 h = ofGetHeight();
 
+ofFbo::Settings settings;
+settings.width = w;
+settings.height = h;
+
+fbo.allocate(settings);
+fbo1.allocate(w,h);
+fbo1.getTexture().getTextureData().bFlipTexture = true;
+
 cam.setPosition(glm::vec3(1.0));
 cam.setLookAt(glm::vec3(0.0));
-
-
 
 seed = ofRandom(0,1);
  
@@ -24,19 +28,32 @@ seed = ofRandom(0,1);
 
 void ofApp::draw() {
 
-shader.begin();
+fbo.begin();
+ofClear();
+buffer.begin();
+buffer.setUniform2f("resolution",w,h);
+buffer.setUniform1f("seed",seed);
+buffer.end();
+fbo.end();
 
+fbo1.begin();
+render.begin();
 
-shader.setUniform1f("time",ofGetElapsedTimeMillis()/1000.);
-shader.setUniform1f("frame",ofGetLastFrameTime());
-shader.setUniform2f("resolution",w,h);
-shader.setUniform1f("seed",seed);
-shader.setUniform3f("camPos",glm::vec3(cam.getPosition()));
+cam.begin();
+render.setUniform1f("time",ofGetElapsedTimeMillis()/1000.);
+render.setUniform1f("frame",ofGetLastFrameTime());
+render.setUniform2f("resolution",w,h);
+render.setUniform1f("seed",seed);
+render.setUniform3f("camPos",glm::vec3(cam.getPosition()));
+render.setUniformTexture("tex",fbo.getTexture(0),1);
 
 ofDrawRectangle(0,0,w,h);
+cam.end();
 
-shader.end();
 
+render.end();
+fbo1.end();
+fbo1.draw(0,0,w,h);
 
 }
 
@@ -63,6 +80,6 @@ void ofApp::keyPressed(int key) {
             src_frag = res.getPath();
         }
         
-        shader.load(path/"render.vert",src_frag);
+        render.load(path/"render.vert",src_frag);
     } 
 }
