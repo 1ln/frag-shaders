15fcc81d1592d901927cde409786c309aa1d8643
dan olson <dano7900@gmail.com>
2021-06-27 16:02:34 -0400

    update

--- src/concentric-extrusion.glsl
+++ src/concentric-extrusion.glsl
@@ -15,12 +15,6 @@ float eps = 0.00001;
 float dmin = 0.;
 float dmax = 750.;
 const int aa = 2;
- 
-const int shsteps = 45; 
-float shblur = 10.0;
-float shmax = 100.; 
-
-const int aosteps = 5;
 
 const int octaves = 5;
 float hurst = 0.5;
@@ -66,7 +60,7 @@ float f2(vec2 x) {
     return s;
 }
 
-mat2 rot2(float a) {
+mat2 rot(float a) {
 
     float c = cos(a);
     float s = sin(a);
@@ -94,18 +88,21 @@ float extr(vec3 p,float d,float h) {
 
 }
 
+float sdif(float d1,float d2,float k) {
+    float h = clamp(.5-.5*(d2+d1)/k,0.,1.);
+    return mix(d2,-d1,h) +k*h +(1.-h);
+}
 
 vec2 scene(vec3 p) {
 
     vec2 res = vec2(1.,0.);
 
-    float d = 0.;     
+    float d = 0.;
 
-    d = extr(p,sin(length(p.xy)*2.),10.5);
-
-    res = opu(res,vec2(d,2.)); 
-  
+    d = extr(p,sin(length(p.xy)*3.),1.5);
+    res = opu(res,vec2(sdif(d,p.z,.5)*.5,2.));  
     return res;
+
 
 }
 
@@ -131,40 +128,23 @@ vec2 rayScene(vec3 ro,vec3 rd) {
 
 }
 
-float calcAO(vec3 p,vec3 n) {
-
-    float o = 0.;
-    float s = 1.;
-
-    for(int i = 0; i < aosteps; i++) {
- 
-        float h = .01 + .125 * float(i) / 4.; 
-        float d = scene(p + h * n).x;  
-        o += (h-d) * s;
-        s *= .9;
-        if(o > .33) break;
-    
-     }
-     return clamp(1. - 3. * o ,0.0,1.0) * (.5+.5*n.y);   
-}
-
 float shadow(vec3 ro,vec3 rd) {
 
     float res = 1.0;
     float t = 0.005;
     float ph = 1e10;
     
-    for(int i = 0; i < shsteps; i++ ) {
+    for(int i = 0; i < 95; i++ ) {
         
         float h = scene(ro + rd * t  ).x;
 
         float y = h * h / (2. * ph);
         float d = sqrt(h*h-y*y);         
-        res = min(res,shblur * d/max(0.,t-y));
+        res = min(res,125. * d/max(0.,t-y));
         ph = h;
         t += h;
     
-        if(res < eps || t > shmax) { break; }
+        if(res < eps || t > 5.) { break; }
 
         }
 
@@ -190,7 +170,7 @@ vec3 renderScene(vec3 ro,vec3 rd) {
  
 vec2 d = rayScene(ro, rd);
 
-vec3 col = vec3(0.45) * max(0.,rd.y);
+vec3 col = vec3(1.);
 
 if(d.y >= 0.) {
 
@@ -201,7 +181,7 @@ vec3 l = normalize(vec3(10.));
 vec3 h = normalize(l - rd);
 vec3 r = reflect(rd,n);
 
-col += vec3(f2(p.xz+f2(p.yz)));
+col += vec3(f2(p.xz+f2(p.yx*3.)*5.));
 
 float amb = clamp(0.5 + 0.5 * n.y,0.,1.);
 
@@ -211,18 +191,15 @@ float fre = pow(clamp(1. + dot(n,rd),0.0,1.0),2.0);
 float ref = smoothstep(-.2,.2,r.y);
 
 vec3 linear = vec3(0.);
-
+ 
 dif *= shadow(p,l);
 ref *= shadow(p,r);
 
 linear += dif * vec3(.5);
 linear += amb * vec3(0.01,0.05,0.05);
-linear += ref * vec3(10.);
-linear += fre * vec3(0.25,0.5,0.35);
 
 col = col * linear;
- 
-col = mix(col,vec3(0.45),1.-exp(-0.000001 * d.x*d.x*d.x*d.x)); 
+col = mix(col,vec3(1.),1.-exp(-0.00001 *d.x*d.x*d.x)); 
 
 }
 
@@ -232,8 +209,10 @@ return col;
 void main() {
  
 vec3 color = vec3(0.);
-vec3 ro = vec3(25.,-5.,15.);
+vec3 ro = vec3(10.,10.,25.);
 vec3 ta = vec3(0.0);
+
+ro.xy *= rot(time*.1);
 
 for(int k = 0; k < aa; ++k) {
     for(int l = 0; l < aa; ++l) {
@@ -246,15 +225,15 @@ for(int k = 0; k < aa; ++k) {
     mat3 cm = camOrthographic(ro,ta,0.);
     vec3 rd = cm * normalize(vec3(uv.xy,2.));
 
-    vec3 render = renderScene(ro,rd);    
+    vec3 col = renderScene(ro,rd);    
 
-    color += render;
+    col = pow(col,vec3(.4545));
+    color += col;
     }
+}
 
 color /= float(aa*aa);
-color = pow(color,vec3(.4545));
-
 FragColor = vec4(color,1.0);
-}
+
 
 }
