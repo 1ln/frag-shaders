99184d6afb947470c47643f0d50f8b812d85cddf
dan olson <dano7900@gmail.com>
2021-07-11 17:36:06 -0400

    update

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -28,7 +28,12 @@ gui.setup();
 gui.add(seed.set("seed",(int)(ofRandom(0,1)*1000000)));
 gui.add(info.set("info",false));
 
-
+key_up = false;
+key_down = false;
+key_right = false;
+key_left = false;
+key_x = false;
+key_z = false;
 
 }
 
@@ -53,7 +58,15 @@ render.setUniform1f("frame",ofGetLastFrameTime());
 render.setUniform2f("resolution",w,h);
 render.setUniform1i("seed",seed);
 render.setUniform3f("camPos",glm::vec3(cam.getPosition()));
+render.setUniform1i("up",key_up);
+render.setUniform1i("down",key_down);
+render.setUniform1i("right",key_right);
+render.setUniform1i("left",key_left);
+render.setUniform1i("key_x",key_x);
+render.setUniform1i("key_z",key_z);
 render.setUniformTexture("tex",fbo.getTexture(0),1);
+
+
 
 ofDrawRectangle(0,0,w,h);
 
@@ -75,6 +88,30 @@ h = ofGetHeight();
 }
 
 void ofApp::keyPressed(int key) {
+
+    if(key == OF_KEY_UP) {
+        key_up = true;
+    }
+ 
+    if(key == OF_KEY_DOWN) {
+        key_down = true;
+    }
+
+    if(key == OF_KEY_RIGHT) {
+        key_right = true;
+    }
+
+    if(key == OF_KEY_LEFT) {
+        key_left = true;
+    }
+
+    if(key == 'x') {
+        key_x = true;
+    }
+
+    if(key == 'z') {
+        key_z = true;
+    }
 
     if(key == 'g') {
         img.grabScreen(0,0,w,h);
