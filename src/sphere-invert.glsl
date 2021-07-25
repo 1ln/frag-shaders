eb774e357afc621ebdae8a8516246568101aac18
dan olson <dano7900@gmail.com>
2021-07-25 01:10:22 -0400

    update

--- src/sphere-invert.glsl
+++ src/sphere-invert.glsl
@@ -10,16 +10,18 @@ uniform float time;
 uniform int seed;
 
 const int steps = 250;
-float eps = 0.001;
+float eps = 0.00001;
 float dmin = 0.001;
 float dmax = 24;
 const int aa = 2;
  
 const int shsteps = 45; 
-float shblur = 10.0;
-float shmax = 100.; 
+float shblur = 100.;
+float shmax = 3.; 
 
 vec3 ta = vec3(0.,0.5,3.);
+vec3 ta2 = vec3(.5,-2.,10.);
+vec3 ta3 = vec3(1.,.5,-5.);
 
 const float PI2 = radians(180.)*2.;
 
@@ -27,6 +29,10 @@ float h11(float p) {
     uvec2 n = uint(int(p)) * uvec2(uint(int(seed)),2531151992.0);
     uint h = (n.x ^ n.y) * uint(int(seed));
     return float(h) * (1./float(0xffffffffU));
+}
+
+mat2 rot(float a) {
+    return mat2(cos(a),-sin(a),sin(a),cos(a));
 }
 
 mat3 rotAxis(vec3 axis,float theta) {
@@ -90,11 +96,16 @@ vec2 scene(vec3 p) {
 
     vec2 res = vec2(1.,0.);
 
-    float d = 0.;     
-    d = -(length(p)-12.);
+    float d = 0.; 
 
-    res = opu(res,vec2(d,2.)); 
+    vec3 q = p; 
+    q.xz *= rot(time*.01);   
+    d = -(length(q)-21.5);
+
+    res = opu(res,vec2(d,25.)); 
     res = opu(res,vec2(length(p-ta)-.25,1.));
+    res = opu(res,vec2(length(p-ta2)-1.,2.));
+    res = opu(res,vec2(length(p-ta3)-.5,3.));
 
     return res;
 
@@ -174,19 +185,46 @@ if(d.y >= 0.) {
 
 vec3 p = ro + rd * d.x;
 vec3 n = calcNormal(p);
-vec3 l = normalize(ta-vec3(.25));
-float radius = dot(rd,l);
-col += col * vec3(.05,.25,.16)*expStep(radius,75.);
-col += col * vec3(.5,.25,.1)*expStep(radius,100.);
-col += col * vec3(.25)*expStep(radius,25.);
-
-
-vec3 h = normalize(l - rd);
-vec3 r = reflect(rd,n);
 
 if(d.y == 1.) {
-    col = vec3(.001);
+    col = vec3(.5);
 }
+
+if(d.y == 2.) {
+    col = vec3(1.,.5,.25);
+}
+
+if(d.y == 3.) {
+    col = vec3(1.);
+}
+
+if(d.y == 25.) {
+    col = vec3(.5);
+}
+
+vec3 l = normalize(ta-vec3(.25));
+float rad = dot(rd,l);
+col += col * vec3(.05,.25,.16)*expStep(rad,75.);
+col += col * vec3(.5,.25,.1)*expStep(rad,100.);
+col += col * vec3(.25)*expStep(rad,25.);
+col += col * vec3(1.,.5,.5)*expStep(rad,100.);
+
+vec3 la = normalize(ta2-vec3(2.,10.,-5.));
+float rad1 = dot(rd,la);
+col += col * vec3(.25,.89,.01)*expStep(rad1,25.);
+col += col * vec3(1.)*expStep(rad1,112.);
+col += col * vec3(.25)*expStep(rad1,131.);
+col += col * vec3(.5)*expStep(rad1,16.);
+
+vec3 lb = normalize(ta3-vec3(-5.));
+float rad2 = dot(rd,lb);
+col += col * vec3(1.,.12,.5)*expStep(rad2,100.);
+col += col * vec3(.5,.25,.95)*expStep(rad2,50.);
+col += col * vec3(.05,.35,.0025)*expStep(rad2,45.);
+col += col * vec3(1.)*expStep(rad2,25.);
+
+vec3 h = normalize(l - rd);
+vec3 r = reflect(rd,n);}
 
 float amb = clamp(0.5 + 0.5 * n.y,0.,1.);
 
@@ -203,13 +241,11 @@ vec3 linear = vec3(0.);
 dif *= shadow(p,l);
 ref *= shadow(p,r);
 
-linear += .05+ dif * vec3(0.5,0.24,0.34);
-linear += .02+ amb * vec3(0.01,0.05,0.05);
-linear += .01+ ref * vec3(0.1,.05,0.45);
-linear += .01+ fre * vec3(0.25,0.005,0.0035);
+linear += .05+ dif * vec3(0.5);
+linear += .02+ amb * vec3(.005);
 
 col = col * linear;
-col += 3.+ spe * vec3(0.01,.05,.025); 
+col += spe * vec3(.025); 
 col = mix(col,vec3(1.),1.-exp(-0.00001 * d.x*d.x*d.x)); 
 
 }
